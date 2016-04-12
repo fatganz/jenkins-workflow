e9acd5189c91b3ec363301b6012c166f38a57922
Rostyslav Kinash <rkinash@rebbix.com>
2016-04-12 12:56:26 +0300

    added some automation tests

--- ci/scripts/feature-pipeline.groovy
+++ ci/scripts/feature-pipeline.groovy
@@ -3,6 +3,8 @@ std = load 'ci/scripts/std.groovy'
 def go(String branchName) {
   print "working with $branchName"
   print "doing feature testing pipeline";
+  stage "testing"
+  std.runTests()
 
   stage "preview"
   node {}
--- ci/scripts/production-pipeline.groovy
+++ ci/scripts/production-pipeline.groovy
@@ -1,6 +1,12 @@
+std = load 'ci/scripts/std.groovy'
+
 def go(String branchName) {
   print "working with $branchName"
   print "doing production pipeline";
+  
+  stage "testing"
+  std.runTests()
+
   stage "qa"
   node {}
 
