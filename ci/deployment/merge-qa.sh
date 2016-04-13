65aa133f4174f73a3437736f4b8c621084579dac
Rostyslav Kinash <rkinash@rebbix.com>
2016-04-13 11:05:42 +0300

    Proper tagging

--- ci/deployment/merge-qa.sh
+++ ci/deployment/merge-qa.sh
@@ -12,9 +12,9 @@ if [ "qa" == "$BRANCH_NAME" ]; then
 fi
 git fetch --all
 git checkout $BRANCH_NAME
+git tag -a "feature-${BUILD_NUMBER}" -m "feature-${BUILD_NUMBER}"
 git checkout qa
 git merge $BRANCH_NAME
 git commit -m "Merging branch ${BRANCH_NAME} into QA"
-git tag -a "feature-${BUILD_NUMBER}" -m "feature-${BUILD_NUMBER}"
 git push origin qa
 git push origin "feature-${BUILD_NUMBER}"
