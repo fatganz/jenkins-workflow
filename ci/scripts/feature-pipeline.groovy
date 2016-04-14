453df1539567475e26df5d2c063653901a9700d5
Rostyslav Kinash <rkinash@rebbix.com>
2016-04-14 18:58:39 +0300

    preview

--- ci/scripts/feature-pipeline.groovy
+++ ci/scripts/feature-pipeline.groovy
@@ -1,27 +1,35 @@
 #!groovy
 
 utils = load 'ci/scripts/utils.groovy'
+branchName = utils.normalizeBranchName(env.BRANCH_NAME)
 
 def go(String branchName) {
   print "working with $branchName"
   print "doing feature testing pipeline";
   stage "testing"
-  node {
-      utils.runTests()
-  }
+  def tstImg = docker.build("toastme/app-test:snapshot", 'app/preview')
+
   stage "preview"
-  node {
-    sh "ci/deployment/deploy-ft.sh"
+  try{
+    tstImg.stop()
+  } catch(e) {
   }
+  tstImg.run("--name "${branchName}"")
 
   stage "complete"
   input message: "Feature complte?", ok: "Yes"
 
-  node {
-    sshagent (credentials: ['5cfc7cca-6168-4848-b3ef-9aa628a780bd']) {
-      sh 'ci/deployment/merge-feature.sh'
-    }
-  }
+  // node {
+  //   sshagent (credentials: ['5cfc7cca-6168-4848-b3ef-9aa628a780bd']) {
+  //     sh 'ci/deployment/merge-feature.sh'
+  //   }
+  // }
+  tstImg.stop();
+}
+
+def hostIp(container) {
+  sh "docker inspect -f '{{ .NetworkSettings.IPAddress }}' ${container.id} > hostIp_${env.BUILD_TAG}"
+  readFile("hostIp_${env.BUILD_TAG}").trim()
 }
 
 return this
