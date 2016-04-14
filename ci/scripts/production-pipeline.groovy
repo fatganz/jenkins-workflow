cdb0c83989ffb89361e7c349e73e8eaea60abbde
Rostyslav Kinash <rkinash@rebbix.com>
2016-04-14 15:04:21 +0300

    let's try to check local port

--- ci/scripts/production-pipeline.groovy
+++ ci/scripts/production-pipeline.groovy
@@ -12,7 +12,7 @@ def go(String branchName) {
   node {
     def pcImg = docker.build("toastme/app:${env.BUILD_NUMBER}", 'app')
     pcImg.withRun("-p 8090:8080"){ c ->
-      sh "curl http://localhost:8090"
+      sh "curl http://localhost:8080"
     }
     sshagent (credentials: ['5cfc7cca-6168-4848-b3ef-9aa628a780bd']) {
       sh 'ci/deployment/merge-qa.sh'
