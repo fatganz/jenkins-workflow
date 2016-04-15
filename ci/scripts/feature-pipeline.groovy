f5abfe150a16111e283c012cf2d3c8b90d462e9b
Rostyslav Kinash <rkinash@rebbix.com>
2016-04-15 15:33:21 +0300

    add version

--- ci/scripts/feature-pipeline.groovy
+++ ci/scripts/feature-pipeline.groovy
@@ -6,6 +6,7 @@ def go(String branchName) {
   print "Faeture testing $branchName"
   stage "testing"
   node {
+    writeFile file:'app/web/version.php', text:"<?php define('VERSION', '${env.BUILD_TAG}')"
     sh "docker build -f app/Dockerfile.preview -t toastme/app-test:snapshot ."
   }
 
