9f0d265154d6274f16d7bceb4a4cf7eaabb91b82
Rostyslav Kinash <rkinash@rebbix.com>
2016-04-18 14:30:56 +0300

    map instead of copy

--- ci/scripts/feature-pipeline.groovy
+++ ci/scripts/feature-pipeline.groovy
@@ -10,7 +10,11 @@ def go(String branchName) {
     checkout scm
     sh "docker run --rm -v ${pwd()}/app:/app composer/composer:latest install"
     sh "docker run -v ${pwd()}/app:/app phpunit/phpunit --bootstrap vendor/autoload.php tests/"
-    withEnv(["COMPOSE_VIRTUAL_HOST=${branchName}.qa.toastme.internal", "BUILD_TAG=${env.BUILD_TAG}"]){
+    withEnv([
+      "COMPOSE_VIRTUAL_HOST=${branchName}.qa.toastme.internal",
+      "BUILD_TAG=${env.BUILD_TAG}"
+      "COMPOSE_VOLUME_DIR=${pwd()}/app"
+      ]){
       sh "docker-compose -p $branchName -f docker-compose-feature.yml up -d --build"
     }
     // sh "docker build -f app/Dockerfile.preview -t toastme/app-test:$branchName-snapshot ."
